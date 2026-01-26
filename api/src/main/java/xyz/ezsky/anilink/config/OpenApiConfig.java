473832983cbc2aa8052d85a9704a877f4a1c8c4f
ezsky333 <ezsky333@gmail.com>
2026-01-26 07:48:25 +0000

    【修改】(配置): 优化OpenAPIDefinition注解格式

--- api/src/main/java/xyz/ezsky/anilink/config/OpenApiConfig.java
+++ api/src/main/java/xyz/ezsky/anilink/config/OpenApiConfig.java
@@ -2,15 +2,12 @@ package xyz.ezsky.anilink.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 import org.springframework.context.annotation.Configuration;
 
-
 @Configuration
-@OpenAPIDefinition(info = @Info(
-        title = "AniLinkService API 文档",
-        version = "1.0.0",
-        description = "基于弹弹play开放平台搭建的番剧识别与弹幕播放器"
-))
+@OpenAPIDefinition(info = @Info(title = "AniLinkService API 文档", version = "1.0.0", description = "基于弹弹play开放平台搭建的番剧识别与弹幕播放器"), servers = @Server(url = "/", description = "默认服务器地址"))
 public class OpenApiConfig {
-    
+
 }
